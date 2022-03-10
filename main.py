ee4529ee3b1978cde86c4847dd29b6a27efa9c77
Matthew Andrews <95978192+Matthew-Andrews03@users.noreply.github.com>
2022-03-09 20:47:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
+# Matt's Lidar Branch
 
-print("I think this is working?") #tell me if you can see this - Cole
-
-#yes
-#here
-
-#Carter are you reading me?
-
-#matt testing
-
-#matt testing branch
