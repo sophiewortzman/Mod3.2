4ec56f37dbe3c1bcf85015c6e3924d099fea666b
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-13 14:56:19 -0400

    Update MainAlgorithm.py

--- MainAlgorithm.py
+++ MainAlgorithm.py
@@ -16,11 +16,17 @@ def main():
         for dist in rover.laser_distances:
             if dist < 0.1:
                 print("TOO CLOSE")
+                left_side_speed = 0
+                right_side_speed = 0     
         rover.send_command(left_side_speed, right_side_speed)
         i = i + 1
         sleep(0.01)
 
         #run dijkstra's algorithm
+        #after dijkstra's, start moving again
+         left_side_speed = 1
+         right_side_speed = 1     
+         rover.send_command(left_side_speed, right_side_speed)
 
 if __name__ == "__main__":
     main()
