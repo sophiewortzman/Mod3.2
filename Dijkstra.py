1f0b19d51f51d9e5d747f3782e07ff54f25b364c
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-13 14:57:29 -0400

    Update Dijkstra.py

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,2 +1,8 @@
 #Implement dijkstra's algorithm
 print("Implement Dijkstra's Algorithm with available code\n")
+
+
+#after dijkstra's, start moving again
+         left_side_speed = 1
+         right_side_speed = 1     
+         rover.send_command(left_side_speed, right_side_speed)
