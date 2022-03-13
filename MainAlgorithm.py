2fe7b780652c77484899cd6a2177db2419342d15
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-13 14:52:43 -0400

    Create MainAlgorithm.py

---
+++ MainAlgorithm.py
@@ -0,0 +1,26 @@
+
+   
+from qset_lib import Rover
+from time import sleep
+
+def main():
+    rover = Rover()
+
+    i = 0
+
+    left_side_speed = 1
+    right_side_speed = 1
+
+    while i < 1000:
+        print("X: " + rover.x + " Y: " + rover.y + " Heading: " + rover.heading)
+        for dist in rover.laser_distances:
+            if dist < 0.1:
+                print("TOO CLOSE")
+        rover.send_command(left_side_speed, right_side_speed)
+        i = i + 1
+        sleep(0.01)
+
+        #run dijkstra's algorithm
+
+if __name__ == "__main__":
+    main()
