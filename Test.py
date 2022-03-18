2373db47af860fe7fd57c99d50b1e9f9dd26d619
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-18 14:13:40 -0400

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,11 @@
+from qset_lib import Rover
+from time import sleep
+
+def main():
+    rover = Rover()
+
+    i = 0
+
+    left_side_speed = 1
+    right_side_speed = 1
+    rover.send_command(left_side_speed, right_side_speed)
