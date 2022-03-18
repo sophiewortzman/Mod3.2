fcc7f5a8f4941843540fd727e94b6e72f9609b77
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-18 14:21:16 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,4 +5,8 @@ def main():
     rover = Rover()
     left_speed = 1
     right_speed = 1
-    rover.send_command(left_speed, right_speed)
+    def send_command(self, left_speed, right_speed):
+        msg = wheelSpeed()
+        msg.left = left_speed
+        msg.right = right_speed
+        self.__cmd_pub.publish(msg)
