63537aec2b3127c07408df07cf12dc2f78794cfa
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-18 19:04:35 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,8 +1,112 @@
+#start of code snippets
 from qset_lib import Rover
 from time import sleep
 
 def main():
     rover = Rover()
-    left_speed = 1
-    right_speed = 1
-    rover.send_command(left_speed, right_speed)
+
+import rospy  # this is the module required for all simulation communication
+    i = 0
+
+# start of wheel control code
+from wheel_control.msg import wheelSpeed  # this is a required module for the drive communication
+    left_side_speed = 1
+    right_side_speed = 1
+
+rospy.init_node("controller")
+    while i < 1000:
+        print("X: " + rover.x + " Y: " + rover.y + " Heading: " + rover.heading)
+        for dist in rover.laser_distances:
+            if dist < 0.1:
+                print("TOO CLOSE")
+        rover.send_command(left_side_speed, right_side_speed)
+        i = i + 1
+        sleep(0.01)
+
+class WheelController:
+
+    def __init__(self):
+        self.wheel_pub = rospy.Publisher("/gazebo_wheelControl/wheelSpeedTopic", wheelSpeed, queue_size=1)
+
+    def drive_wheels(self, left, right):
+        # type: (float, float) -> None
+        # left and right are numbers between -1 and 1
+        msg = wheelSpeed()
+        msg.left = left
+        msg.right = right
+        msg.wheelMode = 0
+        self.wheel_pub.publish(msg)
+        #print(msg)
+
+
+# end of wheel control code
+
+# start of laser scan code
+from sensor_msgs.msg import LaserScan
+
+
+class LaserListener:
+
+    def __init__(self):
+        self.laserSub = rospy.Subscriber("/leddar/leddarData", LaserScan, self.laser_callback, queue_size=1)
+        self.laserRanges = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+    def laser_callback(self, msg):
+        # type: (LaserScan) -> None
+        self.laserRanges = msg.ranges
+
+
+# end of laser scan code access laserRanges for an array of all measured distances from the laser sensors
+
+# start of localization stuff
+from geometry_msgs.msg import Point
+from std_msgs.msg import Float32
+
+
+class LocationHeading:
+
+    def __init__(self):
+        self.fixSub = rospy.Subscriber("/fix/metres", Point, self.fix_callback, queue_size=1)
+        self.headingSub = rospy.Subscriber("/heading",Float32, self.heading_callback, queue_size=1)
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+        self.heading = 0.0
+
+    def fix_callback(self, msg):
+        # type: (Point) -> None
+        self.x = msg.x
+        self.y = msg.y
+        self.z = msg.z
+
+    def heading_callback(self, msg):
+        # type: (Float32) -> None
+        self.heading = msg.data
+
+
+# end of localization stuff
+
+
+#initiallize classes to get and send data to gazebo
+locHead  = LocationHeading()
+laser = LaserListener()
+wheel = WheelController()
+#end of initialization
+
+# start of control loop snippet
+
+while not rospy.is_shutdown():  #this will run until gazebo is shut down or CTRL+C is pressed in the ubuntu window that is running this code
+    minRange = 99 #initialize minRange to a value larger than what will be recieved
+    for x in range(0, 15): #iterate through the ranges list
+        if laser.laserRanges[x] < minRange: #if the current range is smaller than the smallest know range
+            minRange = laser.laserRanges[x] #update the range
+    if minRange < 3: #if there is something closer than 3m infront of the rover
+        wheel.drive_wheels(1, -1) #turn
+    else:
+        wheel.drive_wheels(1, 1) #go staright
+    print("Current Heading: ", locHead.heading, "Current x val: ", locHead.x, "RightMostLaser: ", laser.laserRanges[0]) #print some random data to the command line
+
+# end of control loop snippet
+if __name__ == "__main__":
+    main()
+
