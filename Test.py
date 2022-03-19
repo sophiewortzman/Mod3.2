bbf4dc4ca466c3314fd9543a35210a12b6b6ec49
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-18 21:00:45 -0400

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,25 @@
+from qset_lib import Rover
+from time import sleep
+
+def main():
+    rover = Rover()
+
+    i = 0
+
+    left_side_speed = -5
+    right_side_speed = 5
+
+    while i < 1000:
+        #print("X: " + rover.x + " Y: " + rover.y + " Heading: " + rover.heading)
+        for dist in rover.laser_distances:
+            if dist < 0.5:
+                left_side_speed = 0
+                right_side_speed = 0
+                rover.send_command(left_side_speed, right_side_speed)
+        rover.send_command(left_side_speed, right_side_speed)
+        i = i + 1
+        sleep(0.01)
+
+
+if __name__ == "__main__":
+    main()
