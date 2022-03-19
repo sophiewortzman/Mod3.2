cb07743b5a93121f84f658f5df281011c41d176c
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-18 21:01:32 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,8 +6,8 @@ def main():
 
     i = 0
 
-    left_side_speed = -5
-    right_side_speed = 5
+    left_side_speed = 4
+    right_side_speed = 4
 
     while i < 1000:
         #print("X: " + rover.x + " Y: " + rover.y + " Heading: " + rover.heading)
