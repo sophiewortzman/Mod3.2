18330b34ae12d2666085e3b60ef947c7b220c3e2
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-18 21:20:01 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -13,7 +13,7 @@ def main():
         #print("X: " + rover.x + " Y: " + rover.y + " Heading: " + rover.heading)
         #from sensor_msgs.msg import LaserScan
         for dist in rover.laser_distances:
-            if dist < 0.5:
+            if dist < 1:
                 left_side_speed = 0
                 right_side_speed = 0
                 rover.send_command(left_side_speed, right_side_speed)
