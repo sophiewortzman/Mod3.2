145742dd4ec273cbb3df0c4b061dbd0663ef7879
dermottduggan <99502023+dermottduggan@users.noreply.github.com>
2022-03-20 13:27:40 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -23,8 +23,8 @@ def main():
                     right_side_speed = 1
                     rover.send_command(left_side_speed, right_side_speed)
                     sleep(0.01)
+                    print(str(rover.heading))
                 break
-                print(str(rover.heading))
                 left_side_speed = 0
                 right_side_speed = 0
                 rover.send_command(left_side_speed, right_side_speed)
