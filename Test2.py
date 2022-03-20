a47d9e8bf37441d69efee516c6dbb2b4532a425a
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-20 15:48:52 -0400

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -15,7 +15,6 @@ def main():
     while not rospy.is_shutdown():
         print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading))
         print (rover.laser_distances)
-        print("left_side_speed: " + str(left_side_speed) + "right_side_speed: " + str(right_side_speed))
         for dist in rover.laser_distances:
             if dist < 1.5:
                 if left_side_speed == 5:
@@ -32,6 +31,7 @@ def main():
                     left_side_speed = 1
                     right_side_speed = -1
                     rover.send_command(left_side_speed, right_side_speed)
+                    print("left_side_speed: " + str(left_side_speed) + "right_side_speed: " + str(right_side_speed))
                 if rover.heading == Wall:
                     left_side_speed = 5
                     right_side_speed = 5
