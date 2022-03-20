675b1fe0db0eb79e5cc550e26fd3d23162686d92
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-20 16:34:06 -0400

    Create Test3.py

---
+++ Test3.py
@@ -0,0 +1,38 @@
+from qset_lib import Rover
+from time import sleep
+import rospy
+
+def main():
+    rover = Rover()
+
+    i = 0
+    Wall = 0
+
+    left_side_speed = 5
+    right_side_speed = 5
+    rover.send_command(left_side_speed, right_side_speed)
+
+    while not rospy.is_shutdown():
+        print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading))
+        print (rover.laser_distances)
+        
+        for dist in rover.laser_distances:
+            left_side_speed = 5
+            right_side_speed = 5
+            rover.send_command(left_side_speed, right_side_speed)
+            
+            if dist < 1.5:
+                left_side_speed = -1
+                right_side_speed = 1
+                rover.send_command(left_side_speed, right_side_speed) 
+                sleep(1)
+                left_side_speed = 1
+                right_side_speed = -1
+                rover.send_command(left_side_speed, right_side_speed)
+                sleep(1)
+       
+        sleep(0.05)
+
+
+if __name__ == "__main__":
+    main()
