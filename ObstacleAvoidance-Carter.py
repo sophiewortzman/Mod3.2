2baa97cde1500dd61fc07ec447d85003428f8b08
moorecarter <99502217+moorecarter@users.noreply.github.com>
2022-03-24 12:43:58 -0400

    Update ObstacleAvoidance-Carter.py

--- ObstacleAvoidance-Carter.py
+++ ObstacleAvoidance-Carter.py
@@ -1,80 +1,107 @@
-from qset_lib import Rover
-from time import sleep
-import rospy
+def main():
+    rover = Rover()
 
-rover = Rover()
+import rospy  # this is the module required for all simulation communication
+    i = 0
 
-def turn_left(rover, left_speed, right_speed):
-    temp = rover.heading
-    while(1):
-        left_side_speed = -1
-        right_side_speed = 1
+# start of wheel control code
+from wheel_control.msg import wheelSpeed  # this is a required module for the drive communication
+    left_side_speed = 1
+    right_side_speed = 1
+
+rospy.init_node("controller")
+    while i < 1000:
+        print("X: " + rover.x + " Y: " + rover.y + " Heading: " + rover.heading)
+        for dist in rover.laser_distances:
+            if dist < 0.1:
+                print("TOO CLOSE")
         rover.send_command(left_side_speed, right_side_speed)
-        # Here is where you would place the desired heading variable.
-        if rover.heading > temp + 90:
-            left_side_speed = 0
-            right_side_speed = 0
-            rover.send_command(left_side_speed, right_side_speed)
-        break
-        sleep(0.05)
-        
-def turn_right(rover, left_speed, right_speed):
-  temp = rover.heading
-  while(1):
-      left_side_speed = 1
-      right_side_speed = -1
-      rover.send_command(left_side_speed, right_side_speed)
-      # Here is where you would place the desired heading variable.
-      if rover.heading < temp - 90:
-          left_side_speed = 0
-          right_side_speed = 0
-          rover.send_command(left_side_speed, right_side_speed)
-      break
-  sleep(0.05)
+        i = i + 1
+        sleep(0.01)
 
-def reset_heading(rover, left_side_speed, right_side_speed):
-    temp = rover.heading
-    for dist in rover.laser_distances:
-        if dist > 10:
-            while(1):
-                left_side_speed = 1
-                right_side_speed = -1
-                rover.send_command(left_side_speed, right_side_speed)
-      # Here is where you would place the desired heading variable.
-     def __init__(self)
+class WheelController:
+
+    def __init__(self):
+        self.wheel_pub = rospy.Publisher("/gazebo_wheelControl/wheelSpeedTopic", wheelSpeed, queue_size=1)
+
+    def drive_wheels(self, left, right):
+        # type: (float, float) -> None
+        # left and right are numbers between -1 and 1
+        msg = wheelSpeed()
+        msg.left = left
+        msg.right = right
+        msg.wheelMode = 0
+        self.wheel_pub.publish(msg)
+        #print(msg)
+
+
+# end of wheel control code
+
+# start of laser scan code
+from sensor_msgs.msg import LaserScan
+
+
+class LaserListener:
+
+    def __init__(self):
         self.laserSub = rospy.Subscriber("/leddar/leddarData", LaserScan, self.laser_callback, queue_size=1)
         self.laserRanges = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
-    def laser_callback(self, msg)
+    def laser_callback(self, msg):
         # type: (LaserScan) -> None
         self.laserRanges = msg.ranges
 
 
-def main():
+# end of laser scan code access laserRanges for an array of all measured distances from the laser sensors
 
-    while not rospy.is_shutdown():
-        left_side_speed = 5
-        right_side_speed = 5
-        rover.send_command(left_side_speed, right_side_speed)
-        
-        
-        print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading))
-        print (rover.laser_distances)
-        print(self.laserRanges)
-        for dist in rover.laser_distances:
-               if dist < 5:
-                turn_right(rover, left_side_speed, right_side_speed)
-#                if left_side_speed == 5:
-#                    Wall = rover.heading
-#                left_side_speed = -1
-#                right_side_speed = 1
-#                rover.send_command(left_side_speed, right_side_speed)
-#            if rover.heading > Wall + 90:
-#                left_side_speed = 0
-#                right_side_speed = 0
-#                rover.send_command(left_side_speed, right_side_speed)
-    sleep(0.05)
+# start of localization stuff
+from geometry_msgs.msg import Point
+from std_msgs.msg import Float32
 
 
+class LocationHeading:
+
+    def __init__(self):
+        self.fixSub = rospy.Subscriber("/fix/metres", Point, self.fix_callback, queue_size=1)
+        self.headingSub = rospy.Subscriber("/heading",Float32, self.heading_callback, queue_size=1)
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+        self.heading = 0.0
+
+    def fix_callback(self, msg):
+        # type: (Point) -> None
+        self.x = msg.x
+        self.y = msg.y
+        self.z = msg.z
+
+    def heading_callback(self, msg):
+        # type: (Float32) -> None
+        self.heading = msg.data
+
+
+# end of localization stuff
+
+
+#initiallize classes to get and send data to gazebo
+locHead  = LocationHeading()
+laser = LaserListener()
+wheel = WheelController()
+#end of initialization
+
+# start of control loop snippet
+
+while not rospy.is_shutdown():  #this will run until gazebo is shut down or CTRL+C is pressed in the ubuntu window that is running this code
+    minRange = 99 #initialize minRange to a value larger than what will be recieved
+    for x in range(0, 15): #iterate through the ranges list
+        if laser.laserRanges[x] < minRange: #if the current range is smaller than the smallest know range
+            minRange = laser.laserRanges[x] #update the range
+    if minRange < 3: #if there is something closer than 3m infront of the rover
+        wheel.drive_wheels(1, -1) #turn
+    else:
+        wheel.drive_wheels(1, 1) #go staright
+    print("Current Heading: ", locHead.heading, "Current x val: ", locHead.x, "RightMostLaser: ", laser.laserRanges[0]) #print some random data to the command line
+
+# end of control loop snippet
 if __name__ == "__main__":
     main()
