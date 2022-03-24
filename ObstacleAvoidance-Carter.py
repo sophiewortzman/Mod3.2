0b0295e4029a61b78d14ee3a276ca0137595cc1b
moorecarter <99502217+moorecarter@users.noreply.github.com>
2022-03-24 11:10:14 -0400

    Update ObstacleAvoidance-Carter.py

--- ObstacleAvoidance-Carter.py
+++ ObstacleAvoidance-Carter.py
@@ -1,1 +1,13 @@
 #Carter's Code for implemeneting obstacle avoidance
+from qset_lib import Rover
+from time import sleep
+import rospy
+
+def main():
+  distances = []
+  rover=Rover()
+  while (1):
+    distances = rover.laser_distances.copy()
+    print (distances(1))
+  
+  
