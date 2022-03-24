8c59b2069e02c6e6edfbaed5d2352b04c1a3f449
moorecarter <99502217+moorecarter@users.noreply.github.com>
2022-03-24 11:12:34 -0400

    Update ObstacleAvoidance-Carter.py

--- ObstacleAvoidance-Carter.py
+++ ObstacleAvoidance-Carter.py
@@ -1,11 +1,62 @@
-#Carter's Code for implemeneting obstacle avoidance
-from qset_lib import Rover
-from time import sleep
-import rospy
+def turn_left(rover, left_speed, right_speed):
+    temp = rover.heading
+        while(1):
+            left_side_speed = -1
+            right_side_speed = 1
+            rover.send_command(left_side_speed, right_side_speed)
+            # Here is where you would place the desired heading variable.
+            if rover.heading > temp + 90:
+                left_side_speed = 0
+                right_side_speed = 0
+                rover.send_command(left_side_speed, right_side_speed)
+                break
+        sleep(0.05)
+        
+  def turn_right(rover, left_speed, right_speed):
+    temp = rover.heading
+        while(1):
+            left_side_speed = 1
+            right_side_speed = -1
+            rover.send_command(left_side_speed, right_side_speed)
+            # Here is where you would place the desired heading variable.
+            if rover.heading < temp - 90:
+                left_side_speed = 0
+                right_side_speed = 0
+                rover.send_command(left_side_speed, right_side_speed)
+                break
+        sleep(0.05)
+
+    i = 0
+    Wall = 0        
+
 
 def main():
-  distances = []
-  rover=Rover()
-  while (1):
-    print(rover.laser_distances(1))  
+    rover = Rover()
+    
   
+   
+  
+
+    
+
+      while not rospy.is_shutdown():
+        left_side_speed = 5
+        right_side_speed = 5
+        rover.send_command(left_side_speed, right_side_speed)
+        
+        
+        print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading))
+        print (rover.laser_distances(1))
+        for dist in rover.laser_distances:
+            if dist < 1.5:
+                turn_left
+#                 if left_side_speed == 5:
+#                     Wall = rover.heading
+#                 left_side_speed = -1
+#                 right_side_speed = 1
+#                 rover.send_command(left_side_speed, right_side_speed)
+#             if rover.heading > Wall + 90:
+#                 left_side_speed = 0
+#                 right_side_speed = 0
+#                 rover.send_command(left_side_speed, right_side_speed)
+        sleep(0.05)
