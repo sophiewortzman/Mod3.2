ad2892196f529568294df8e514787c88e9a9521b
moorecarter <99502217+moorecarter@users.noreply.github.com>
2022-03-24 13:45:07 -0400

    Update Hola.py

--- Hola.py
+++ Hola.py
@@ -62,7 +62,7 @@ def main():
         
         
         print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading))
-        print (rover.laser_distances)
+        print (laser.laserRanges[0])
         for dist in rover.laser_distances:
                if dist < 5:
                 turn_right(rover, left_side_speed, right_side_speed)
