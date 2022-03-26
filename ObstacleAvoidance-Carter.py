4639a01937a2da2a469c0ea8d565bf4851d7d694
moorecarter <99502217+moorecarter@users.noreply.github.com>
2022-03-26 15:58:30 -0400

    Update ObstacleAvoidance-Carter.py

--- ObstacleAvoidance-Carter.py
+++ ObstacleAvoidance-Carter.py
@@ -12,10 +12,14 @@ y=
 def get_heading(x,y):
     distanceX = x -rover.x
     distanceY = y - rover.y
-    heading = math.arctan(distanceY/distanceX)
-    
-    
-
+    heading = math.atan(distanceY/distanceX)
+    heading = heading * (180/(math.pi))
+    if heading < 0:
+        turn_right(rover, left_side_speed, right_side_speed)
+    if heading > 0:
+        turn_left(rover, left_side_speed, right_side_speed)
+    if distanceX == 0 and distanceY == 0:
+        print("DONE!!")
     
 def main():
     rover = Rover()
