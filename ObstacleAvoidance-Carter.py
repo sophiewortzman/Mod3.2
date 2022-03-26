fa41c6f758898ff4f58ef7a78f8216e060e55a8f
moorecarter <99502217+moorecarter@users.noreply.github.com>
2022-03-26 15:48:28 -0400

    Update ObstacleAvoidance-Carter.py

--- ObstacleAvoidance-Carter.py
+++ ObstacleAvoidance-Carter.py
@@ -6,11 +6,15 @@ from wheel_control import wheelSpeed
 #find out how to use data from lidar array
 from sensor_msgs import LaserScan
 laser = LaserListener()
-def __init__(self):
-    self.laserSub = rospy.Subscriber("/leddar/leddarData", LaserScan, self.laser_callback, queue_size=1)
-    self.laserRanges = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-def laser_callback(self, msg):
-    self.laserRanges = msg.ranges
+#HARD CODE COORDINATES
+x=
+y=
+def get_heading(x,y):
+    distanceX = x -rover.x
+    distanceY = y - rover.y
+    heading = math.arctan(distanceY/distanceX)
+    
+    
 
     
 def main():
