89355920167b8d628e4cf40e85e76179b48f3ddf
Matthew-Andrews03 <matthewa0903@gmail.com>
2022-03-25 23:08:07 -0400

    c

--- LiDAR-Matthew.py
+++ LiDAR-Matthew.py
@@ -63,11 +63,10 @@ def find_heading(rover, heading, objective):
     return math.atan(m) * 180 / math.pi  
 
 #call this before obstacle avoidance to find which way is the best to turn (returns "left" or "right")
-def side_to_favour(rover, laser_distances):
+def side_to_favour():
     
     sumRight = 0
     sumLeft = 0
-    index = 0
     count = 0
 
     while(count <= 29):
@@ -110,15 +109,18 @@ def main():
         
         print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading))
         print (rover.laser_distances)
+        
         for dist in rover.laser_distances:
                if dist < 2:
                 
-                    if side_to_favour(rover, rover.laser_distances) == "right":
-                        turn_right(rover, left_side_speed, right_side_speed)
+                whichWay = side_to_favour
+
+                if whichWay == "right":
+                    turn_right(rover, left_side_speed, right_side_speed)
                         
                         
-                    if side_to_favour(rover, rover.laser_distances) == "left":
-                        turn_left(rover, left_side_speed, right_side_speed)
+                if whichWay == "left":
+                    turn_left(rover, left_side_speed, right_side_speed)
                         
 
 #                if left_side_speed == 5:
