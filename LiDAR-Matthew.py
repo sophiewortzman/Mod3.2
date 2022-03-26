451c762e4ed6c2ce0a6a20c458380dff13675ef4
Matthew Andrews <95978192+Matthew-Andrews03@users.noreply.github.com>
2022-03-26 16:14:43 -0400

    Update LiDAR-Matthew.py

--- LiDAR-Matthew.py
+++ LiDAR-Matthew.py
@@ -63,7 +63,7 @@ def reset_heading(rover, left_side_speed, right_side_speed, find_heading):
 
       # Here is where you would place the desired heading variable.
                 #if rover.heading == range(int(find_heading(rover, objectivex, objectivey))-1, int(find_heading(rover, objectivex, objectivey))+1):
-                if (int(find_heading)-1 <= rover.heading <= int(find_heading)+1)
+                if (int(find_heading)-1 <= rover.heading <= int(find_heading)+1):
                     left_side_speed = 0
                     right_side_speed = 0
                     rover.send_command(left_side_speed, right_side_speed)
