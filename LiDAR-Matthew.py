57c61aab81581afce1fbbe3c7ffdd99a0be4f0c7
Matthew Andrews <95978192+Matthew-Andrews03@users.noreply.github.com>
2022-03-27 13:39:55 -0400

    Update LiDAR-Matthew.py

--- LiDAR-Matthew.py
+++ LiDAR-Matthew.py
@@ -134,8 +134,8 @@ def main():
         
         if (rover.x == objectivex) and (rover.y == objectivey):
             left_side_speed = 0 
-        right_side_speed = 0
-        rover.send_command(left_side_speed, right_side_speed)
+            right_side_speed = 0
+            rover.send_command(left_side_speed, right_side_speed)
         
         left_side_speed = 2 
         right_side_speed = 2
