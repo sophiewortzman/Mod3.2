c8dd18902342406471b3e362a7f8bfb9358e19b5
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-27 13:19:22 -0400

    Update LiDAR-Matthew.py

--- LiDAR-Matthew.py
+++ LiDAR-Matthew.py
@@ -59,10 +59,10 @@ def reset_heading(rover, left_side_speed, right_side_speed, find_heading):
             
             tempHeading = find_heading(rover, objectivex, objectivey)
             
-            if (tempHeading-1 >= rover.heading >= tempHeading+1):
-                left_side_speed = 1
-                right_side_speed = -1
-                rover.send_command(left_side_speed, right_side_speed)
+          
+            left_side_speed = 1
+            right_side_speed = -1
+            rover.send_command(left_side_speed, right_side_speed)
                 
             
            
