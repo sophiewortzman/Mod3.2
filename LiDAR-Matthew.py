514040b9e7fb3d21589a1f6be73941d9967b4be3
dermottduggan <99502023+dermottduggan@users.noreply.github.com>
2022-03-31 10:41:00 -0400

    Update LiDAR-Matthew.py

--- LiDAR-Matthew.py
+++ LiDAR-Matthew.py
@@ -167,8 +167,8 @@ def main():
                 reset_heading(rover, left_side_speed, right_side_speed, find_heading)
                 
            
-            while ((objectivex - 0.5) < self.x < (objectivex + 0.5)):
-                while ((objectivey - 0.5) < self.y < (objectivey + 0.5)):
+            while ((objectivex - 0.5) < rover.x < (objectivex + 0.5)):
+                while ((objectivey - 0.5) < rover.y < (objectivey + 0.5)):
                     left_side_speed = 0
                     right_side_speed = 0
                     rover.send_command(left_side_speed, right_side_speed)
