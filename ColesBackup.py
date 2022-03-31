b3080ae6cdc7a6b1bd80ea367ba750d3e66ccd7a
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-31 13:20:55 -0400

    Update ColesBackup.py

--- ColesBackup.py
+++ ColesBackup.py
@@ -66,8 +66,8 @@ def reset_heading(rover, left_side_speed, right_side_speed, tempHeading):
         rover.send_command(left_side_speed, right_side_speed)
                
     if (tempHeading+1<rover.heading<179.99):
-        left_side_speed = -1
-        right_side_speed = 1
+        left_side_speed = 1
+        right_side_speed = -1
         rover.send_command(left_side_speed, right_side_speed)
              
             
