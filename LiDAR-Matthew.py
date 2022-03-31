7bb6976e438a7300a882d92ecadd19d866d1590e
dermottduggan <99502023+dermottduggan@users.noreply.github.com>
2022-03-31 11:10:59 -0400

    Update LiDAR-Matthew.py

--- LiDAR-Matthew.py
+++ LiDAR-Matthew.py
@@ -61,10 +61,11 @@ def reset_heading(rover, left_side_speed, right_side_speed, find_heading):
             
            
             
-            if (tempHeading-2>=rover.heading) or (rover.heading>=tempHeading+2):
-                left_side_speed = 1
-                right_side_speed = -1
-                rover.send_command(left_side_speed, right_side_speed)
+            
+            left_side_speed = 1
+            right_side_speed = -1
+            rover.send_command(left_side_speed, right_side_speed)
+            if (tempHeading-2>=rover.heading) or (rover.heading>=tempHeading+2):    
                 break
                 
                 
