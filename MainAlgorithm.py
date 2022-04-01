94e869f792b752340d3e3f1673f22fe3783e1708
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-31 21:37:42 -0400

    Update MainAlgorithm.py

--- MainAlgorithm.py
+++ MainAlgorithm.py
@@ -153,7 +153,7 @@ def main():
 
         for dist in rover.laser_distances:
             
-            if dist < 5:
+            if dist < 3:
                 whichWay = side_to_favour()
                 #print(whichWay)
 
