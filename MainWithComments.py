8762411e3d4bdfeae277ff123f5c13f65ab86674
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-04-01 15:20:21 -0400

    Update MainWithComments.py

--- MainWithComments.py
+++ MainWithComments.py
@@ -148,8 +148,12 @@ def main():
                 return 0
 
         for dist in rover.laser_distances:
-            # If the rover is within 2 m of an obstacle, decide which way to turn and turn that way until the obstacle is no longer in the way.
-            if rover.laser_distances[13] < 4 or rover.laser_distances[14] < 4 or rover.laser_distances[15] < 4 or rover.laser_distances[16] < 4:
+            # If the rover is within 4 m of an obstacle in front, decide which way to turn and turn that way until the obstacle is no longer in the way.
+            if (rover.laser_distances[13] < 4 or rover.laser_distances[14] < 4 or rover.laser_distances[15] < 4 or rover.laser_distances[16] < 4
+               or rover.laser_distances[0] < 1.1 or rover.laser_distances[2] < 1.1 or rover.laser_distances[4] < 1.1 or rover.laser_distances[6] < 1.1
+               or rover.laser_distances[8] < 1.1 or rover.laser_distances[10] < 1.1 or rover.laser_distances[12] < 1.1 or rover.laser_distances[18] < 1.1
+               or rover.laser_distances[20] < 1.1 or rover.laser_distances[22] < 1.1 or rover.laser_distances[24] < 1.1 or rover.laser_distances[26] < 1.1
+               or rover.laser_distances[28] < 1.1):
                 whichWay = side_to_favour()
                 if whichWay == "right":
                     print("Turning " + whichWay + "...\n")
