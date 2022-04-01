dae8b9e7d6a1a0049609c02736fa70d68d5bf456
colemcewan <99502207+colemcewan@users.noreply.github.com>
2022-03-31 21:02:03 -0400

    Update MainAlgorithm.py

--- MainAlgorithm.py
+++ MainAlgorithm.py
@@ -186,6 +186,7 @@ def main():
                 left_side_speed = 3
                 right_side_speed = 3
                 rover.send_command(left_side_speed, right_side_speed)
+                sleep(1)
                 tempHeading = find_heading(rover, objectivex, objectivey)
                 reset_heading(rover, left_side_speed, right_side_speed, tempHeading)
 
